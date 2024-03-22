798255d366ca62b4e92ab55a6fc8db0c07d6d091
dwd <wenyi173@gmail.com>
2024-03-22 21:23:00 +0800

    test transferSpl

--- transfer_slp.js
+++ transfer_slp.js
@@ -1,16 +1,34 @@
 import {getOrCreateAssociatedTokenAccount, createTransferInstruction} from "@solana/spl-token";
 import {
-    Connection,
-    Keypair,
-    PublicKey,
-    sendAndConfirmTransaction,
-    Transaction
+    Connection, Keypair, PublicKey, sendAndConfirmTransaction, Transaction
 } from "@solana/web3.js";
 import {homedir} from "os";
 import * as fs from "fs";
+import {Wallet} from "@project-serum/anchor";
+import bs58 from "bs58";
 
-async function transferSpl(connection, fromPair, toPair, mint, amount) {
+async function transferSpl(connection, fromPair, toAddr, mint, amount) {
+    console.log(`Sending ${amount} ${(mint)} from ${(fromPair.publicKey.toString())} to ${(toAddr)}.`)
+    //Step 1
+    console.log(`1 - Getting Source Token Account`);
+    let sourceAccount = await getOrCreateAssociatedTokenAccount(connection, fromPair, new PublicKey(mint), fromPair.publicKey);
+    console.log(`Source Account: ${sourceAccount.address.toString()}`);
 
+    //Step 2
+    console.log(`2 - Getting Destination Token Account`);
+    let destinationAccount = await getOrCreateAssociatedTokenAccount(connection, fromPair, new PublicKey(mint), new PublicKey(toAddr));
+    console.log(`Destination Account: ${destinationAccount.address.toString()}`);
+
+    //Step 3
+    console.log(`4 - Creating and Sending Transaction`);
+    const tx = new Transaction();
+    tx.add(createTransferInstruction(sourceAccount.address, destinationAccount.address, fromPair.publicKey, amount))
+
+    const latestBlockHash = await connection.getLatestBlockhash('confirmed');
+    tx.recentBlockhash = await latestBlockHash.blockhash;
+    const signature = await sendAndConfirmTransaction(connection, tx, [fromPair]);
+    console.log('\x1b[32m', //Green Text
+        `   Transaction Success!🎉`, `\n    https://explorer.solana.com/tx/${signature}?cluster=devnet`);
 }
 
 async function getNumberDecimals(connection, mintAddress) {
@@ -24,6 +42,16 @@ const RPC = 'https://solana-mainnet.g.alchemy.com/v2/' + ALCHEMY_API_KEY;
 
 const connect = new Connection(RPC)
 
-getNumberDecimals(connect, "ukHH6c7mMyiWCf1b9pnWe25TSpkDDt3H5pQZgZ74J82").then((value) => console.log(value))
+// getNumberDecimals(connect, "ukHH6c7mMyiWCf1b9pnWe25TSpkDDt3H5pQZgZ74J82").then((value) => console.log(value))
+
+const KEY_FILE = homedir() + '/key/solana/arbi1';
+
+function readPrivateKey() {
+    return fs.readFileSync(KEY_FILE, 'utf8');
+}
+
+const fromPair = new Wallet(Keypair.fromSecretKey(bs58.decode(readPrivateKey())));
+
+transferSpl(connect, fromPair, "F7GgZyEtov9PdaU8mHN8fzxPRBewCe6gzoqUMsbUxqLU", "ukHH6c7mMyiWCf1b9pnWe25TSpkDDt3H5pQZgZ74J82", 300000000).then()
 
 
