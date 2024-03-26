c6dc8034f9aed6395ed0adedbd17518cee6b854b
dwd <wenyi173@gmail.com>
2024-03-26 21:04:16 +0800

    fix transferSpl

--- transfer_slp.js
+++ transfer_slp.js
@@ -26,6 +26,26 @@ async function transferSpl(connection, fromPair, toAddr, mint, amount) {
     tx.add(createTransferInstruction(sourceAccount.address, destinationAccount.address, fromPair.publicKey, amount));
     console.log("Transfer Instruction Created");
 
+    const latestBlockHash = await connection.getLatestBlockhash('confirmed');
+    tx.recentBlockhash = await latestBlockHash.blockhash;
+
+    const versionedTx = VersionedTransaction.deserialize(tx.serialize());
+
+    // Step 4
+    console.log(`4 - Signing Transaction`);
+    versionedTx.sign([fromPair.payer])
+    console.log("Transaction Signed")
+
+    //Step 5
+    console.log(`5 - Sending Transaction`);
+    const rawTransaction = versionedTx.serialize();
+    const sendResult = await connection.sendRawTransaction(rawTransaction, {
+        skipPreflight: true, maxRetries: 2, preflightCommitment: "processed"
+    });
+    console.log(`Transaction Send`);
+
+    return sendResult;
+
     // TODO
 
     // //Step 4
@@ -46,12 +66,12 @@ async function transferSpl(connection, fromPair, toAddr, mint, amount) {
     // return sendResult;
 
     //Step 4
-    console.log(`4 - Sending Transaction`)
-    const latestBlockHash = await connection.getLatestBlockhash('confirmed');
-    tx.recentBlockhash = await latestBlockHash.blockhash;
-    const signature = await sendAndConfirmTransaction(connection, tx, [fromPair.payer]);
-    console.log('\x1b[32m', //Green Text
-        `   Transaction Success!🎉`, `\n    https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    // console.log(`4 - Sending Transaction`)
+    // const latestBlockHash = await connection.getLatestBlockhash('confirmed');
+    // tx.recentBlockhash = await latestBlockHash.blockhash;
+    // const signature = await sendAndConfirmTransaction(connection, tx, [fromPair.payer]);
+    // console.log('\x1b[32m', //Green Text
+    //     `   Transaction Success!🎉`, `\n    https://explorer.solana.com/tx/${signature}?cluster=devnet`);
 }
 
 async function getNumberDecimals(connection, mintAddress) {
