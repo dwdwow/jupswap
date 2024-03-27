4a9a3d61f7efe19c654fda32ed31c7535c53da94
dwd <wenyi173@gmail.com>
2024-03-27 15:59:52 +0800

    debug

--- transfer_slp.js
+++ transfer_slp.js
@@ -69,7 +69,7 @@ console.log("Wallet", fromPair.publicKey.toString());
 const bnSelfWalletAddr = "F7GgZyEtov9PdaU8mHN8fzxPRBewCe6gzoqUMsbUxqLU";
 const bnHotWalletAddr = "5tzFkiKscXHK5ZXCGbXZxdw7gTjjD1mBwuoFbhUvuAi9";
 
-transferSpl(connect, fromPair, bnSelfWalletAddr, "ukHH6c7mMyiWCf1b9pnWe25TSpkDDt3H5pQZgZ74J82", 1_000000).then();
+transferSpl(connect, fromPair, bnHotWalletAddr, "ukHH6c7mMyiWCf1b9pnWe25TSpkDDt3H5pQZgZ74J82", 1_000000).then();
 
 const hostname = '127.0.0.1';
 const port = 3001;
